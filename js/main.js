026a7a00d4a15e71df2a4f2828e85ff412a14ee6
adam wood <a_wood60810@fanshaweonline.ca>
2019-03-26 16:50:12 -0400

    finished home layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+(function() {
+	console.log("Javascript is linked up");
 
+	var button = document.querySelector("#menuBut");
+	var burgerCon = document.querySelector("#burgerCon");
+
+	function hamburgerMenu() {
+		console.log("button clicked");
+		// add class or toggle to button and burgerCon
+		burgerCon.classList.toggle("slideToggle");
+	}
+
+	button.addEventListener("click", hamburgerMenu);
+
+})();
